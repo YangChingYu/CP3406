84126dc80f37601a75fefb0c166d4700ea1c1025
YangChingYu <kevin5296@gmail.com>
2020-01-09 03:16:36 +0800

    1

---
+++ app/src/main/java/com/example/canvasclock/ClockView.java
@@ -0,0 +1,71 @@
+package com.example.canvasclock;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class ClockView extends View {
+
+    private int height, width = 0;
+    private int padding = 0;
+    private int fontSize = 0;
+    private int numeralSpacing = 0;
+    private int handTruncation, hourHandTruncation = 0;
+    private int radius = 0;
+    private Paint paint;
+    private boolean isInit;
+    private int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12};
+    private Rect rect = new Rect();
+    public ClockView(Context context) {
+        super(context);
+    }
+
+    public ClockView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ClockView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    private void initClock() {
+        height = getHeight();
+        width = getWidth();
+        padding = numeralSpacing + 50;
+        fontSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 13, getResources().getDisplayMetrics());
+        int min = Math.min(height,width);
+        radius = min /2 - padding;
+        hourHandTruncation = min/7;
+        paint = new Paint();
+        isInit = true;
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (!isInit) {
+            initClock();
+        }
+        canvas.drawColor(Color.BLACK);
+//        back ground color is black
+        drawCircle(canvas);
+        drawCenter(canvas);
+        drawNumberal(canvas);
+        drawHands(canvas);
+        postInvalidateDelayed(500);
+        invalidate();
+
+    }
+
+    private void drawCircle(Canvas canvas) {
+        paint.reset();
+        paint.setColor(getResources().getColor(android.R.color.white));
+
+    }
+}
